b84747f072b20e00e464077b2a8f72b2c0b28c7c
diesilveira <53955407+diesilveira@users.noreply.github.com>
2019-10-04 19:32:52 -0300

    agregue guante y gema

--- src/Library/items/Guante.cs
+++ src/Library/items/Guante.cs
@@ -1,7 +1,8 @@
+using System;
 namespace RoleplayGame.Items
 {
     /// <summary>
-    /// Sword. Permite atacar.
+    /// Guante Por si solo no hace nada, pero si se combina con gemas sirve para atacar
     /// </summary>
     public class Guante : IAttackItem
     {
@@ -9,12 +10,40 @@ namespace RoleplayGame.Items
         /// El poder de ataque
         /// </summary>
         /// <value></value>
+        /// <summary>
+        /// ataque: se inicia en 0
+        /// </summary>
+        public int ataque = 0;
+        /// <summary>
+        /// gemas se inician en cero
+        /// </summary>
+        public int gemas = 0;
         public int AttackPower
         {
             get
             {
-                return 45;
+                return ataque;
             }
+        }
+
+        public void Agregar_Gema(int gema)
+        {
+            if (gema < 0)
+            {
+                Console.WriteLine("Ingrese un numero positivo de gemas");
+            }
+
+            if (gemas + gema < 5)
+            {
+                ataque = ataque + 20 * gema;
+                gemas = gemas + gema;
+                
+            }
+            else
+            {
+                Console.WriteLine("Excede número de gemas posibles");
+            }
+
         }
 
         public override string ToString()
